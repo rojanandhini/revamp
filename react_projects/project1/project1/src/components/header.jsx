a6de68f02483f954114a6e32ff52ccb9083c7a97
Roja Nandhini <rojanandhini.t@gmail.com>
2026-02-22 17:06:02 +0400

    rolled new updates to portfolio, foodstore and reac app

---
+++ react_projects/project1/project1/src/components/header.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className="flex items-center h-screen bg-gray-50 dark:bg-gray-900">
+  <div className="w-full max-w-screen-xl px-4 mx-auto lg:px-12">
+    {/* Start coding here */}
+    <div className="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 md:rounded-lg">
+      <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+        <button
+          type="button"
+          className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-lg md:w-auto bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+        >
+          <svg
+            className="h-3.5 w-3.5 mr-2 -ml-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+            />
+          </svg>
+          Add product
+        </button>
+        <div
+          className="inline-flex flex-col w-full rounded-md shadow-sm md:w-auto md:flex-row"
+          role="group"
+        >
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-t-lg md:rounded-tr-none md:rounded-l-lg hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-2 focus:ring-primary-700 focus:text-primary-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-primary-500 dark:focus:text-white"
+          >
+            Positions
+          </button>
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 border-x md:border-x-0 md:border-t md:border-b hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-2 focus:ring-primary-700 focus:text-primary-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-primary-500 dark:focus:text-white"
+          >
+            Estimated Traffic
+          </button>
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-gray-200 border-x md:border-x-0 md:border-l md:border-b hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-2 focus:ring-primary-700 focus:text-primary-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-primary-500 dark:focus:text-white"
+          >
+            Visibility
+          </button>
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-b-lg md:rounded-bl-none md:rounded-r-lg hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-2 focus:ring-primary-700 focus:text-primary-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-primary-500 dark:focus:text-white"
+          >
+            All for flowbite.com
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default Header
---
+++ react_projects/project1/project1/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+import Header from './components/header'
+const index = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default index
